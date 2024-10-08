b8d1396e0ce94c0dbd45a7849af36b3d1df67df9
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-10-08 17:58:20 +0300

    final commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
+    print(f"File '{file_name}.txt' created successfully.")
+    f.close()
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
+        print(f"Appended content to '{file_name}.txt' successfully.")
+        f.close()
     pass
 
 def read_file(file_name):
+    with open(f'{file_name}.txt', 'r') as f:
+        file_content_read = f.read()
+    return file_content_read
     pass
